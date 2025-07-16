f80c75c5ee34ce567e4780e49f2518d08f443e5f
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-07-16 14:48:47 +0530

    Add TotalLapTime function to calculate lap times
    Commented out the old number() function and introduced variables for three lap times. Added TotalLapTime() to sum the laps and log the total time in seconds.

--- script.js
+++ script.js
@@ -22,9 +22,21 @@
 
 // start() 
 
-function number() {
-  console.log(42);
+// function number() {
+//   console.log(42);
+  
+// }
+
+// number()
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function TotalLapTime() {
+  let TotalTime = lap1 + lap2 + lap3;
+  console.log(TotalTime,"seconds");
   
 }
 
-number()
+TotalLapTime()
